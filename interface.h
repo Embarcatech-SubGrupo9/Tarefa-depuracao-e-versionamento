018bc806616beede1824c0775a1486a38facf8a3
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-12-23 18:05:12 -0300

    A classe de velocidade foi colocado na interface.

--- interface.h
+++ interface.h
@@ -5,13 +5,11 @@ void menu_Massa();       // ok
 void menu_Temperatura(); // ok
 void menu_Volume();      // ok
 void menu_Tempo();       // Declara a função para o menu de volume
-/* void menu_Velocidade();  */
-void menu_Temperatura();
-void menu_Area();
-
-void menu_Energia(); // Declara a função para o menu de energia
- 
-void menu_Dados(); // Declara a função para o menu de volume
+void menu_Velocidade();  // ok
+void menu_Temperatura(); // ok
+void menu_Area();        // ok
+void menu_Energia();     // Declara a função para o menu de energia
+void menu_Dados();       // Declara a função para o menu de volume
 /* void menu_comprimento();
  */
 #endif
