0164fb9080fc1de40c1b0f5dd281226f974f805a
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-12-23 15:52:26 -0300

    Squashed commit of the following:
    commit a3be75f982811b3d0fb513a921e29c44751c52ad
    Author: LuisEduardo38 <eduardo38sao@gmail.com>
    Date:   Mon Dec 23 15:40:52 2024 -0300
    
        Implementação da possibilidade de realizar conversão tanto de unidades maiores para menores como de menos para maiores.
    
    commit f1c829f429b39479ec4914b9867f1cbfeeee9ee2
    Author: LuisEduardo38 <eduardo38sao@gmail.com>
    Date:   Mon Dec 23 14:53:23 2024 -0300
    
        Alteração nas variáveis e adição de comentários explicando o código. As variáveis long int foram substituídas devido à sua baixa capacidade de armazenamento de valor com a aplicação exibindo 0 já que os valores estavam ultrapassando o limite do tipo de variável, e foram alteradas para float para evitar esse problema.
    
    commit 7c59a159a26ed7937df25d954c3e019e1424adc7
    Author: LuisEduardo38 <eduardo38sao@gmail.com>
    Date:   Mon Dec 23 13:16:30 2024 -0300
    
        Implementação da função de exibir resultados, e outras pequenas alterações.
    
    commit a729c4ffad19c40b0dbcfbc7cd41f2a016a89222
    Author: LuisEduardo38 <eduardo38sao@gmail.com>
    Date:   Mon Dec 23 12:31:58 2024 -0300
    
        Implementação da função para converte os valores.
    
    commit 342c7e422156a029731ca7d91cc7ac1494364789
    Author: LuisEduardo38 <eduardo38sao@gmail.com>
    Date:   Mon Dec 23 10:08:16 2024 -0300
    
        Alteração ortográfica no comentário, na declaração de variáveis.
    
    commit 0a5bcc9ac3c9c5a3cb38fd478731cc5cdf2a36e9
    Author: LuisEduardo38 <eduardo38sao@gmail.com>
    Date:   Mon Dec 23 10:01:47 2024 -0300
    
        Implementação do menu da aplicação.
    
    commit 857332ed634ceea1b1d66ae1bf343c2dcf8b8b20
    Author: LuisEduardo38 <eduardo38sao@gmail.com>
    Date:   Sun Dec 22 11:35:45 2024 -0300
    
        Inicializando meu projeto de dados.

---
+++ dados.c
@@ -0,0 +1,174 @@
+#include <stdio.h>//Biblioteca para entrada/saida
+#include <stdlib.h>//Biblioteca
+
+//Função para converter o tamanho dos dados.
+float calculo(int *UNIDADE, float DADOS){
+    int contado;
+
+    if(UNIDADE[0] > UNIDADE[1]){
+        contado = UNIDADE[0] - UNIDADE[1];
+        /*Aqui é subtraindo menos um da variável contado caso UNIDADE[0] seja igual a 1, o que significa que quero essa conversão em bit, mas para chegar nesta unidade é necessário multiplicar por 8 e o for que faça a conversão multiplica por 1024.*/
+        if(UNIDADE[1] == 1){
+            contado--;
+        }
+
+        for(int i = 0; i < contado; i++){
+            DADOS = DADOS * 1024;
+        }
+
+        //Aqui faço a conversão para bit caso seja necessário.
+        if(UNIDADE[1] == 1){
+            DADOS = DADOS * 8;
+        }
+    }
+    else{
+        contado = UNIDADE[1] - UNIDADE[0];
+        printf("%d", contado);
+        if(UNIDADE[1] == 1){
+            contado--;
+        }
+        printf("%d", contado);
+        for(int i = 0; i < contado; i++){
+            DADOS = DADOS / 1024;
+        }
+
+        //Aqui faço a conversão para bit caso seja necessário.
+        if(UNIDADE[1] == 1){
+            DADOS = DADOS / 8;
+        }
+    }
+
+    printf("Dados: %f\n", DADOS);
+    system("pause");
+
+    return DADOS;
+}
+
+//Função para exibir os resultados da aplicação.
+void exibir(int *UNIDADE, float RESULTADO, float DADOS){
+    
+    printf("DADOS DE ENTRADA DO USUÁRIO\n");
+    printf("Unidade     Simbolo   Valor            Tamanho\n");
+    if(UNIDADE[0] == 3){
+        printf("Kilobyte    KB        1024 Bytes       %.0f\n", DADOS);
+    }
+    else if(UNIDADE[0] == 4){
+        printf("Megabyte    MB        1024 Kilobytes   %.0f\n", DADOS);
+    }
+        else if(UNIDADE[0] == 5){
+        printf("GigaByte    GB        1024 Megabytes   %.0f\n", DADOS);
+    }
+        else if(UNIDADE[0] == 6){
+        printf("Perabyte    PB        1024 GigaBytes   %.0f\n", DADOS);
+    }
+        else if(UNIDADE[0] == 7){
+        printf("Exabyte     EB        1024 Perabytes   %.0f\n", DADOS);
+    }
+
+    printf("DADOS DE SAÍDA DA APLICAÇÃO\n");
+    
+    if(UNIDADE[1] == 1){
+        printf("bit         b         (0 ou 1)         %.0f\n", RESULTADO);
+    }
+    else if(UNIDADE[1] == 2){
+        printf("Byte        B         8 bits           %.0f\n", RESULTADO);
+    }
+    else if(UNIDADE[1] == 3){
+        printf("Kilobyte    KB        1024 Bytes       %.0f\n", RESULTADO);
+    }
+    else if(UNIDADE[1] == 4){
+        printf("Megabyte    MB        1024 Kilobytes   %.0f\n", RESULTADO);
+    }
+    else if(UNIDADE[1] == 5){
+        printf("GigaByte    GB        1024 Megabytes   %.0f\n", RESULTADO);
+    }
+    else if(UNIDADE[1] == 6){
+        printf("Perabyte    PB        1024 GigaBytes   %.0f\n", RESULTADO);
+    }
+    else if(UNIDADE[1] == 7){
+        printf("Exabyte     EB        1024 Perabytes   %.0f\n", RESULTADO);
+    }
+}
+
+int main(){
+    float dados, resultado;//Foi utilizado float devido à sua capacidade de armazenamento.
+    int unidade[2];//Para armazenar os dados de unidade para conversão.
+
+    do{
+        system("cls");
+        printf("Tabela de valores de conversão, escolha um valor para converter\n");
+        printf("Escolha  Unidade     Simbolo   Valor\n");
+        printf("1        1 bit       b       (0 ou 1)\n");
+        printf("2        1 Byte      B       8 bits\n");
+        printf("3        1 Kilobyte  KB      1024 Bytes\n");
+        printf("4        1 Megabyte  MB      1024 Kilobytes\n");
+        printf("5        1 GigaByte  GB      1024 Megabytes\n");
+        printf("6        1 Perabyte  PB      1024 GigaBytes\n");
+        printf("7        1 Exabyte   EB      1024 Perabytes\n\n");
+        printf("0 para sair da aplicação.\n");
+        printf("Digite a unidade de medida que você tem: ");
+        scanf("%d", &unidade[0]);
+        
+        //Esse IF serve para sair imediatamente da aplicação.
+        if(unidade[0] != 0){
+            if (unidade[0] != 1){//Esse IF serve para saber se o usuário escolher bit passar diretamente para o tamanho.
+                printf("Digite a unidade de medida que você quer converte: ");
+                scanf("%d", &unidade[1]);
+            }
+            printf("Digite o tamanho: ");
+            scanf("%f", &dados);
+            system("cls");
+
+            switch (unidade[0])
+            {
+                case 1:
+                    printf("DADOS DE SAÍDA DA APLICAÇÃO\n");
+                    printf("Unidade     Simbolo   Valor     Tamanho\n");
+                    printf("bit         b         (0 ou 1)  %.0f\n", dados);
+                    printf("Como bit já é a unidade de menor medida não é possivel converter ela.\n");
+                    system("pause");
+                    break;
+                case 2:
+                    resultado = dados * 8;
+                    printf("DADOS DE SAÍDA DA APLICAÇÃO\n");
+                    printf("Unidade     Simbolo   Valor       Tamanho\n");
+                    printf("Byte        B         8 bits      %.0f\n", resultado);
+                    system("pause");
+                    break;
+                case 3:
+                    resultado = calculo(unidade, dados);//Chamada da função para realizar os cálculos.
+                    exibir(unidade, resultado, dados);//Chamada da função para exibir o resultado.
+                    system("pause");
+                    break;
+                case 4:
+                    resultado = calculo(unidade, dados);
+                    exibir(unidade, resultado, dados);
+                    system("pause");
+                    break;
+                case 5:
+                    resultado = calculo(unidade, dados);
+                    exibir(unidade, resultado, dados);
+                    system("pause");
+                    break;
+                case 6:
+                    resultado = calculo(unidade, dados);
+                    exibir(unidade, resultado, dados);
+                    system("pause");
+                    break;
+                case 7:
+                    resultado = calculo(unidade, dados);
+                    exibir(unidade, resultado, dados);
+                    system("pause");
+                    break;
+                default:
+                    printf("Opção para unidade inválida.\n");
+                    break;
+                /*Foi utilizado tanto case para poder validar o menu de opções, caso contrário teria que
+                utilizar algum laço de repetição algo parecido, fiz essa escolha para deixar o default como
+                opção inválida.*/
+            }
+        }
+    }while(unidade[0] != 0);
+
+    return 0;
+}
